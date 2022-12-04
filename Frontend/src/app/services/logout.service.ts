50e184fb574e1c7a038b5636bf7d040e2378afe3
Rozmaring Flóra <rozmaringflora@hotmail.com>
2022-12-04 14:46:50 +0100

    logout service

--- Frontend/src/app/services/logout.service.ts
+++ Frontend/src/app/services/logout.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-//import { CookieService } from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LogoutService {
-  //constructor(private cookieService: CookieService, private router: Router) {}
+  constructor(private cookieService: CookieService, private router: Router) {}
 
   logout() {
-    /*sessionStorage.clear();
+    sessionStorage.clear();
     this.cookieService.deleteAll();
-    this.router.navigate(['/']);*/
+    this.router.navigate(['/']);
   }
 }
